69a2abc0b7d152a01be967adbcf952ee0eef7c8b
Stanislav Muravyev <smuravye@c1r10s1.42lausanne.ch>
2025-03-11 13:56:40 +0100

    some progress, maybe, idfk

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,12 +3,12 @@
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 	mysqld --user=mysql --bootstrap << END
-		CREATE DATABASE IF NOT EXISTS '$DB' CHARACTER SET utf8 COLLATE utf8_general_ci;
-		CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWD';
-		GRANT ALL PRIVILEGES ON '$DB'.* TO '$DB_USER'@'%;
-		GRANT ALL PRIVILEGES ON '$DB'.* TO 'root'@'%;
-		SET PASSWORD FOR 'root'@'localhost = PASSWORD('$DB_ROOT_PASSWD') ;
-		FLUSH PRIVILEGES ;
+		CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;
+        CREATE USER IF NOT EXISTS \`$DB_USER\`@'%' IDENTIFIED BY '$DB_USER_PASSWD';
+        GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO \`$DB_USER\`@'%';
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+        SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWD');
+        FLUSH PRIVILEGES;
 END
 fi
 
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+echo "Waiting for MariaDB..."
+until /usr/bin/mysqladmin ping -h"${DB_HOST}" --silent; do
+    echo "MariaDB is still starting..."
+    sleep 5
+done
+echo "MariaDB is up - proceeding with WordPress setup."
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
-	./wp-cli.phar core download --allow-root --path=/var/www/wordpress
-    ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root --path=/var/www/wordpress
-    ./wp-cli.phar core install --url=smuravye.42.fr --title=smuravye\'s_wp --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/wordpress
-    ./wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" --user_pass=$WP_USER_PASSWORD --role=author --allow-root --path=/var/www/wordpress
+	php -d memory_limit=512M wp-cli.phar core download \
+        --allow-root \
+        --path=/var/www/html
+
+    ./wp-cli.phar config create \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_USER_PASSWD \
+        --dbhost=$DB_HOST \
+        --allow-root \
+        --path=/var/www/html
+
+    ./wp-cli.phar core install \
+        --url=$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PASS \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root \
+        --path=/var/www/html
+    
+    ./wp-cli.phar user create \
+        "$WP_USER" "$WP_USER_EMAIL" \
+        --user_pass=$WP_USER_PASSWORD \
+        --role=author \
+        --allow-root \
+        --path=/var/www/html
 fi
 
-exec php-fpm82 -F
+exec php-fpm -F

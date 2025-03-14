a75d527375e3896172c432a544e03debd45ae14d
Stanislav Muravyev <smuravye@c1r10s1.42lausanne.ch>
2025-03-14 14:59:03 +0100

    small update

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-	mysqld --user=mysql --bootstrap << END
+	mysqld --user=root --bootstrap << END
 		CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;
         CREATE USER IF NOT EXISTS \`$DB_USER\`@'%' IDENTIFIED BY '$DB_USER_PASSWD';
         GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO \`$DB_USER\`@'%';
